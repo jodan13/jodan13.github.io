7abc09ac6c7127458ef18eff5c4f92cb18023de5
Jodan <Jodan@aaanet.ru>
2019-09-24 19:23:33 +0300

    media

--- js/script.js
+++ js/script.js
@@ -50,3 +50,20 @@ for (let anchor of anchors) {
     });
   });
 }
+window.addEventListener("DOMContentLoaded", () => {
+  const menu = document.querySelector(".menu"),
+    menuItem = document.querySelectorAll(".menu_item"),
+    hamburger = document.querySelector(".hamburger");
+
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("hamburger_active");
+    menu.classList.toggle("menu_active");
+  });
+
+  menuItem.forEach(item => {
+    item.addEventListener("click", () => {
+      hamburger.classList.toggle("hamburger_active");
+      menu.classList.toggle("menu_active");
+    });
+  });
+});

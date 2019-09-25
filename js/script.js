68bab5135398f7965b786ed2cb6357fd3150ee78
Jodan <Jodan@aaanet.ru>
2019-09-25 10:25:24 +0300

    slider

--- js/script.js
+++ js/script.js
@@ -1,40 +1,40 @@
 "use strict";
 
-//  Индекс слайда по умолчанию
-let slideIndex = 1;
-showSlides(slideIndex);
+// //  Индекс слайда по умолчанию
+// let slideIndex = 1;
+// showSlides(slideIndex);
 
-//  Функция увеличивает индекс на 1, показывает следующй слайд
-function plusSlide() {
-  showSlides((slideIndex += 1));
-}
+// //  Функция увеличивает индекс на 1, показывает следующй слайд
+// function plusSlide() {
+//   showSlides((slideIndex += 1));
+// }
 
-//  Функция уменьшяет индекс на 1, показывает предыдущий слайд
-function minusSlide() {
-  showSlides((slideIndex -= 1));
-}
+// //  Функция уменьшяет индекс на 1, показывает предыдущий слайд
+// function minusSlide() {
+//   showSlides((slideIndex -= 1));
+// }
 
-//  Устанавливает текущий слайд
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
+// //  Устанавливает текущий слайд
+// function currentSlide(n) {
+//   showSlides((slideIndex = n));
+// }
 
-// Основная функция сладера
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("portfolio__item");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
+// // Основная функция сладера
+// function showSlides(n) {
+//   let i;
+//   let slides = document.getElementsByClassName("portfolio__item");
+//   if (n > slides.length) {
+//     slideIndex = 1;
+//   }
+//   if (n < 1) {
+//     slideIndex = slides.length;
+//   }
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
 
-  slides[slideIndex - 1].style.display = "flex";
-}
+//   slides[slideIndex - 1].style.display = "flex";
+// }
 
 const anchors = document.querySelectorAll('a[href*="#"]');
 
@@ -58,6 +58,22 @@ window.addEventListener("DOMContentLoaded", () => {
   hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("hamburger_active");
     menu.classList.toggle("menu_active");
+  });
+
+  function addListenerMulti(element, eventNames, listener) {
+    let events = eventNames.split(" ");
+    for (let i = 0, iLen = events.length; i < iLen; i++) {
+      element.addEventListener(events[i], listener, false);
+    }
+  }
+
+  addListenerMulti(window, "scroll touchmove", function() {
+    if (hamburger.classList.contains("hamburger_active")) {
+      hamburger.classList.remove("hamburger_active");
+    }
+    if (menu.classList.contains("menu_active")) {
+      menu.classList.remove("menu_active");
+    }
   });
 
   menuItem.forEach(item => {
